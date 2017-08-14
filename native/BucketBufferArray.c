1d848ff6c9a37a021d0a11c2e01f1786232b6878
Christopher Zell <Zelldon91@gmail.com>
2017-08-14 10:56:17 +0200

    renmae h file and create c file

---
+++ native/BucketBufferArray.c
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "BucketBufferArray.h"
+
+
+JNIEXPORT jlong JNICALL Java_de_zell_jnative_BucketBufferArray_allocate
+  (JNIEnv *env, jobject jobj, jlong size)
+{
+    printf("allocate");
+    return -1L;
+}
+
+JNIEXPORT void JNICALL Java_de_zell_jnative_BucketBufferArray_free
+  (JNIEnv *env, jobject jobj, jlong address)
+{
+    printf("free");
+}
